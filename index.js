e25879e62335b5175e4de3b9e541b9d5c49204a5
ismael <ismaelreyplata@gmail.com>
2024-01-25 20:13:00 +0100

    comprueba email duplicado

--- index.js
+++ index.js
@@ -142,6 +142,10 @@ app.post('/register', (req, res) => {
   //const fechaFormateada = discharge_date.toISOString().slice(0, 19).replace('T', ' ');
   const userId = uuidv4();
   connection.query(INSERT_USER_QUERY, [userId, name, email, tel, password, discharge_date], (err, results) => {
+    if (error.code === 'ER_DUP_ENTRY') {
+      // El error es una entrada duplicada
+      res.status(400).send('La dirección de correo electrónico ya está en uso. Por favor, elija otra.');
+    } 
     if (err) {
       console.error('Error registering user: ', err);
       res.status(500).json({ error: 'Could not register user' });
