89c9b05b15925f70d6bf517eb6628f93da234cbc
ismael <ismaelreyplata@gmail.com>
2024-01-28 16:58:57 +0100

    get perfil user

--- index.js
+++ index.js
@@ -96,10 +96,25 @@ app.post('/api/login', (req, res) => {
       const token = jwt.sign( {user} , JWT_SECRET_KEY, { expiresIn: '1h' });
 
       res.cookie("jwt", token);
+      res.cookie("user", user);
       res.json({ message: 'Inicio de sesión exitoso', token, user });
     }
     
   });
+  
+});
+
+app.get('/api/perfil', (req, res) => {
+  //const { email, password } = req.body;
+  const jwtCookie = req.cookies.jwt;
+  const userCookie = req.cookies.user;
+  if (jwtCookie) {
+      // El usuario tiene una cookie JWT
+      // Haz lo que necesites hacer con ella, como verificarla, decodificarla, etc.
+      res.json({ user:  userCookie });
+  } else {
+      res.json({ message: 'El usuario no tiene una cookie JWT' });
+  }
   
 });
 
